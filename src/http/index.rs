763ab814a4ce8b1fa4b5e17007ba3e08e36a6204
gcxfd <i@rmw.link>
2021-05-21 16:01:46 +0800

    🔶

--- src/http/index.rs
+++ src/http/index.rs
@@ -3,9 +3,13 @@ use std::convert::TryInto;
 use tide::Request;
 
 pub async fn post(mut req: Request<()>) -> tide::Result {
-  let body = req.body_bytes().await?;
+  let id = match kv::id.get(b"ipv4")? {
+    Some(id) => u64::from_be_bytes((&*id).try_into()?),
+    _ => 0,
+  };
 
-  Ok(format!("rmw body {}", String::from_utf8(body)?).into())
+  kv::id.set(b"ipv4", &(id + 1).to_be_bytes());
+  Ok("".into())
 }
 
 pub async fn get(mut req: Request<()>) -> tide::Result {
