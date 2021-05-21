c9a9e9a3bea3e2345d36aee7e4c6f425f7eefcbc
gcxfd <i@rmw.link>
2021-05-21 15:07:57 +0800

    🔶

--- src/http.rs
+++ src/http.rs
@@ -4,12 +4,12 @@ use tide::Request;
 async fn index(mut req: Request<()>) -> tide::Result {
   let body = req.body_bytes().await?;
 
-  Ok(format!("body = {:?}", body).into())
+  Ok(format!("rmw body = {:?}", String::from_utf8(body)).into())
 }
 
 pub async fn listen(addr: String) -> Result<(), Error> {
   let mut app = tide::new();
-  app.at("").get(index);
+  app.at("").get(index).post(index);
   app.listen(addr).await?;
   Ok(())
 }
