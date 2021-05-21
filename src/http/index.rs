f4085ce62cf64da5fd65233ceb2de097ad154b0b
gcxfd <i@rmw.link>
2021-05-21 16:31:54 +0800

    🔶

--- src/http/index.rs
+++ src/http/index.rs
@@ -1,16 +1,25 @@
 use crate::kv;
 use std::convert::TryInto;
-use tide::{Body, Request};
+use tide::{Body, Request, Response};
+
+fn increment(old: Option<&[u8]>) -> Option<Vec<u8>> {
+  let number = match old {
+    Some(bytes) => {
+      let array: [u8; 8] = bytes.try_into().unwrap();
+      let number = u64::from_be_bytes(array);
+      number + 1
+    }
+    None => 1,
+  };
+
+  Some(number.to_be_bytes().to_vec())
+}
 
 pub async fn post(_: Request<()>) -> tide::Result {
-  let id = (match kv::id.get(b"ipv4")? {
-    Some(id) => u64::from_be_bytes((&*id).try_into()?),
-    _ => 0,
-  } + 1)
-    .to_be_bytes();
-
-  kv::id.set(b"ipv4", &id)?;
-  Ok(Body::from_bytes(id.to_vec()).into())
+  Ok(match kv::id.update_and_fetch("ipv4", increment)? {
+    Some(id) => Body::from_bytes(id.to_vec()).into(),
+    _ => Response::new(500),
+  })
 }
 
 pub async fn get(mut req: Request<()>) -> tide::Result {
